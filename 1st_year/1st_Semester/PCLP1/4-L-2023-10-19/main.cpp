aa551e0c3d4a923cada2b6ccab77b2e82fefb12d
TheRealOne78 <bajcsielias78@gmail.com>
2023-10-19 14:28:38 +0300

    PCLP1/4-L-2023-10-19

---
+++ 1st_year/1st_Semester/PCLP1/4-L-2023-10-19/main.cpp
@@ -0,0 +1,39 @@
+// "primul meu program C++"
+#include <iostream>
+
+// using namespace std;
+
+int main () {
+
+    /*
+     * Delectus aliquam necessitatibus molestias. Fuga similique non sunt illum.
+     * Deleniti nostrum placeat perspiciatis deleniti veniam est. Delectus at a
+     * aliquam praesentium alias consequatur assumenda. Ex aut reprehenderit
+     * cupiditate ut magni corrupti. Neque corrupti sed mollitia totam velit
+     * voluptatem est enim.
+     *
+     * Blanditiis a optio consequatur. Optio molestiae commodi atque nisi aut
+     * deserunt. Accusamus voluptas aut sunt animi aperiam. Perspiciatis ea
+     * voluptas quas aliquid minima maxime. Cumque perspiciatis expedita quos
+     * quia. Quasi necessitatibus sint et et quis tempore.
+     *
+     * Consequatur aut omnis quos quis numquam. Odit porro repellat autem cum
+     * perferendis ab quia eveniet. Qui harum corrupti amet.
+     *
+     * Delectus aliquam enim reiciendis voluptatem illo impedit. Consequuntur
+     * voluptatibus ut est dolorem et necessitatibus consequatur. Possimus
+     * aliquam repellat minima rerum ratione nihil. Enim est in facere. Voluptas
+     * facilis et maxime placeat illo sed iure atque. Eum ullam eos delectus
+     * deleniti suscipit accusantium iste.
+     *
+     * Deleniti dolore saepe voluptatem tempora ut et aliquid. Corporis deleniti
+     * quis repudiandae et dolore. Commodi quas nihil quod quia quasi a iusto
+     * iure. Voluptate ducimus neque pariatur consectetur maxime.
+     */
+
+    // This is a one-liner comment
+
+  std::cout << "Bun venit la laboratorul de C++!\n";
+  std::cout << "Acesta este un program C++" << std::endl;
+  return 0;
+}
