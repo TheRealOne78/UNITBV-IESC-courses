87ee2e1664bce249a0103bb156f90a157348ee5e
TheRealOne78 <bajcsielias78@gmail.com>
2024-11-07 21:36:19 +0200

    solve lab

--- 2nd_year/1st_Semester/SDA/04_L_2024_11_01/app/src/main/java/org/clase/OperatiiCurs.java
+++ 2nd_year/1st_Semester/SDA/04_L_2024_11_01/app/src/main/java/org/clase/OperatiiCurs.java
@@ -4,6 +4,6 @@ public interface OperatiiCurs {
     public void UpdateProfesor(Profesor p);
     public void AddStudent(Student student);
     public void RemoveStudent(Student student);
-    public void UpdateStudent(Student student);
+    public void UpdateStudent(Student student_old, Student student_nou);
     public void UpdateCurs(String nume, String descriere);
 }
--- 2nd_year/1st_Semester/SDA/04_L_2024_11_01/app/src/main/java/org/clase/OperatiiManagerCursuri.java
+++ 2nd_year/1st_Semester/SDA/04_L_2024_11_01/app/src/main/java/org/clase/OperatiiManagerCursuri.java
@@ -1,5 +1,7 @@
+package org.clase;
+
 public interface OperatiiManagerCursuri {
     public void AddCurs(Curs curs);
-    public void UpdateCurs(Curs curs);
+    public void UpdateCurs(Curs curs_vechi, Curs curs_nou);
     public void DeleteCurs(Curs curs);
 }
