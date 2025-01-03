bf90d331282aaa1569050e75d0074d7a20d55b47
TheRealOne78 <bajcsielias78@gmail.com>
2025-01-03 03:26:32 +0200

    add a shitton load of shit I don't remember what it does anymore

--- 2nd_year/1st_Semester/SDA/08_L_2024_11_29/app/src/main/java/org/clase/OperatiiCurs.java
+++
@@ -1,9 +0,0 @@
-package org.clase;
-
-public interface OperatiiCurs {
-    public void updateProfesor(Profesor p);
-    public void addStudent(Student student);
-    public void removeStudent(Student student);
-    public void updateStudent(Student student_old, Student student_nou);
-    public void updateCurs(String nume, String descriere);
-}
